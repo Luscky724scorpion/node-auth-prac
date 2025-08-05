cb9295fee751ff0c226175433c7c145112a70761
Luscky724scorpion <songhokhan@gmail.com>
2025-08-05 10:47:42 -0600

    minor fix

--- Routes/admin-routes.js
+++ Routes/admin-routes.js
@@ -3,7 +3,7 @@ const authMiddleware = require("../middlewares/auth-middleware");
 const adminMiddleware = require("../middlewares/admin-middlewares");
 
 const router = express.Router();
-const authMiddleware=require('../middlewares/auth-middleware')
+
 
 router.get("/welcome", authMiddleware, adminMiddleware, (req, res) => {
   res.json({
--- Routes/auth-routes.js
+++ Routes/auth-routes.js
@@ -1,9 +1,9 @@
 const express=require('express')
 const router=express.Router()
 const{registerUser,loginUser}=require('../Controllers/auth-controller')
-const authMiddleware=require("../middlewares/auth-middleware")
+
 //routes related to user auth
 router.post('/register',registerUser)
 router.post('/login',loginUser)
-router.post("./changed-passowrd",changePassword)
+
 module.exports=router
